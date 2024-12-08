7d68001972bbaaadebed3059b7dbf7887449d3a3
abed7helmi <abed.helmi@yahoo.fr>
2024-12-08 23:11:10 +0100

    add back project

---
+++ backend-students-spring/src/main/java/net/helmi/backendstudentsspring/BackendStudentsSpringApplication.java
@@ -0,0 +1,13 @@
+package net.helmi.backendstudentsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStudentsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStudentsSpringApplication.class, args);
+	}
+
+}
---
+++ backend-students-spring/src/test/java/net/helmi/backendstudentsspring/BackendStudentsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.helmi.backendstudentsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudentsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
