875e53e6742ef73189dc12965526bbeaff88dd03
abed7helmi <abed.helmi@yahoo.fr>
2024-12-09 22:40:50 +0100

    couche web

--- backend-students-spring/src/main/java/net/helmi/backendstudentsspring/BackendStudentsSpringApplication.java
+++ backend-students-spring/src/main/java/net/helmi/backendstudentsspring/BackendStudentsSpringApplication.java
@@ -1,13 +1,57 @@
 package net.helmi.backendstudentsspring;
 
+import net.helmi.backendstudentsspring.entites.Payment;
+import net.helmi.backendstudentsspring.entites.PaymentStatus;
+import net.helmi.backendstudentsspring.entites.PaymentType;
+import net.helmi.backendstudentsspring.entites.Student;
+import net.helmi.backendstudentsspring.repository.PaymentRepository;
+import net.helmi.backendstudentsspring.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.UUID;
 
 @SpringBootApplication
 public class BackendStudentsSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendStudentsSpringApplication.class, args);
+	}
+
+
+
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository, PaymentRepository paymentRepository){
+		return args -> {
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString())
+					.code("112233").firstName("Mohamed").programId("GLSID").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString())
+					.code("112244").firstName("Imane").programId("GLSID").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString())
+					.code("112255").firstName("Aymane").programId("BDCC").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString())
+					.code("112266").firstName("Lobna").programId("BDCC").build());
+
+			PaymentType[] paymentTypes = PaymentType.values();
+			Random random=new Random();
+			studentRepository.findAll().forEach(st->{
+				for (int i = 0; i <10 ; i++) {
+					int index = random.nextInt(paymentTypes.length);
+					Payment payment = Payment.builder()
+							.date(LocalDate.now())
+							.amount(1000+(int)(Math.random()*20000))
+							.type(paymentTypes[index])
+							.status(PaymentStatus.CREATED)
+							.student(st)
+							.build();
+					paymentRepository.save(payment);
+				}
+			});
+		};
 	}
 
 }
---
+++ backend-students-spring/src/main/java/net/helmi/backendstudentsspring/web/StudentRestController.java
@@ -0,0 +1,74 @@
+package net.helmi.backendstudentsspring.web;
+
+import net.helmi.backendstudentsspring.entites.Payment;
+import net.helmi.backendstudentsspring.entites.PaymentStatus;
+import net.helmi.backendstudentsspring.entites.Student;
+import net.helmi.backendstudentsspring.repository.PaymentRepository;
+import net.helmi.backendstudentsspring.repository.StudentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//@CrossOrigin("*")
+public class StudentRestController {
+    private StudentRepository studentRepository;
+    private PaymentRepository paymentRepository;
+
+//    private PaymentService paymentService;
+
+    public StudentRestController(StudentRepository studentRepository, PaymentRepository paymentRepository) {
+        this.studentRepository = studentRepository;
+        this.paymentRepository = paymentRepository;
+//        this.paymentService = paymentService;
+    }
+
+    @GetMapping(path = "/students")
+    public List<Student> allStudents(){
+        return studentRepository.findAll();
+    }
+    @GetMapping("/students/{code}")
+    public Student getStudentByCode(@PathVariable String code){
+        return studentRepository.findByCode(code);
+    }
+    @GetMapping(path = "/studentsByProgram")
+    public List<Student> studentsByProgram(@RequestParam String programId){
+        return studentRepository.findByProgramId(programId);
+    }
+
+    @GetMapping("/payments")
+    public List<Payment> allPayments(){
+        return paymentRepository.findAll();
+    }
+    @GetMapping("/payments/{id}")
+    public Payment getPaymentById(@PathVariable Long id){
+        return paymentRepository.findById(id).get();
+    }
+    @GetMapping("/students/{code}/payments")
+    public List<Payment> paymentsByStudentCode(@PathVariable String code){
+        return paymentRepository.findByStudentCode(code);
+    }
+    @GetMapping("/paymentsByStatus")
+    public List<Payment> paymentsByStaus(@RequestParam PaymentStatus status){
+        return paymentRepository.findByStatus(status);
+    }
+    /*@PutMapping("/payments/{paymentId}/updateStatus")
+    public Payment updatePaymentStatus(@RequestParam PaymentStatus status, @PathVariable Long paymentId){
+        return paymentService.updatePaymentStatus(status,paymentId);
+    }
+    @PostMapping(path="/payments", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Payment savePayment(@RequestParam MultipartFile file, double amount, PaymentType type,
+                               LocalDate date, String studentCode) throws IOException {
+        return paymentService.savePayment(file,amount,type,date,studentCode);
+
+    }
+
+    @GetMapping(path="payments/{id}/file",produces = MediaType.APPLICATION_PDF_VALUE)
+    public byte[] getPaymentFile(@PathVariable Long id) throws IOException {
+        return paymentService.getPaymentFile(id);
+
+    }*/
+}
